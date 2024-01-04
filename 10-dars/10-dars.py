814720baebd0a94a07278bb391510fcf80f78fdb
Mirvohid2007 <155615165+Mirvohid2007@users.noreply.github.com>
2024-01-04 13:06:25 +0500

    Yangi fayllar qo'shildi
    10-11-darslar yuklandi

---
+++ 10-dars/10-dars.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  1 18:15:33 2024
+
+@author: mirvohid
+"""
+
+
+
+
+#mashinalar=['tesla','bmw','kia','hyundai','volvo']
+
+#for mashina in mashinalar:
+        #if mashina=='bmw':
+            #print(mashina.upper())
+        #else:
+            #print(mashina.title())
+
+#ism='Ali'
+#ism.lower()=='ali'
+
+
+
+
+#ism=input('Ismingiz nima?\n>>>')
+#if ism.lower()!='ali':
+    #print(f"Uzr {ism.title()} biz Alini kutyapmiz")
+#else:
+    #print('Salom Ali')
+
+
+#son=float(input('5x6 nechiga teng?\n>>>'))
+#if son!=30:
+    #print('Javob xato')
+
+
+#yosh=int(input('Yoshingiz nechida?\n>>>'))
+#if yosh>=18:
+    #print('Xush kelibsiz')
+#else:
+    #print('Uzr hali yoshlik qilasiz')
+
+
+
+
+
+#login=input('Yangi login kiriting >>>')
+#if len(login)>=8:
+    #print('Login qabul qilindi')
+#else:
+    #print('Uzunroq login kiriting')
+
+
+#yosh=int(input('Tug\'ilgan yilingizni kiriting >>>'))
+#if 2024-yosh>=18:
+    #print('Xush kelibsiz')
+#else:
+    #print(f"Siz {2024-yosh} yoshda ekansiz")
+    #print('Kirish mumkin emas')
+
+
+#yosh=int(input('Yoshingizni kiriting:'))
+#if yosh>=65: print('Siz covid-19 risk guruhida ekansiz')
+
+#x, y=25, 3
+#print('x>y') if x>y else print('x<y')
+
+
+
+
+
+
+
+#cars=['toyota','mazda','hyundai','gm','kia']
+#for car in cars:
+    #if car=='gm':
+        #print(car.upper())
+    #else:
+        #print(car.title())
+
+
+
+#cars=['toyota','mazda','hyundai','gm','kia']
+#for car in cars:
+    #if car!='gm':
+        #print(car.title())
+    #else:
+        #print(car.upper())
+
+
+#login=input('Loginni kirting:')
+#if login.lower()=='admin':
+    #print('xush kelibsiz, Admin.Foydalanuvchilar ro\'yxatini ko\'rasizmi?')
+#else:
+    #print(f"Xush kelibsiz {login}!")
+
+
+
+
+
+#raqamlar=[]
+#print('Istagan 2 ta soningizni kiriting ')
+#ason=input('1-sonni kiriting:')
+#bson=input('2-sonni kiriting:')
+#if ason==bson:
+    #print('sonlar teng ekan')
+#else:
+    #print(ason,bson)
+
+
+
+#son=int(input('istalgan sonni kiriting:'))
+#if son<0:
+    #print('Manfiy son')
+#else:
+    #print('Musbat son')
+
+
+#son=int(input('Istalgan son kiritng:'))
+#if son>0:
+    #print(son**(1/2))
+#else:
+    #print('Musbat son kiriting')
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 11-dars.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  2 12:24:09 2024
+
+@author: mirvohid
+"""
+
+
+#yosh=int(input('Yoshingiz nechida?'))
+#if yosh<=4:
+    #print('Sizga kirish bepul')
+#elif yosh<=14:
+    #print('Sizga kirish 5000 so\'m')
+#else:
+    #print('Sizga kirish 10000 so\'m')
+
+
+
+
+
+
+#yosh=int(input('Yoshingiz nechida?'))
+#if yosh<=4:
+    #narx=0
+#elif yosh<=14:
+    #narx=5000 
+#else:
+    #narx=10000
+#print(f"Sizga kirish {narx} so'm")
+
+
+
+
+
+
+
+#kun=input('Bugun qaysi hafta kuni? ')
+#if kun.lower()=='shanba' or kun.lower()=='yakshanba':
+    #print('Bugun dam olish kuni')
+#else:
+    #print('bugun ish kuni')
+
+
+
+
+
+
+
+
+#kun=input('bugun qaysi hafta kuni? ')
+#havo=int(input('Havo harorati qanday? '))
+#if kun.lower()=='yakshanba' or kun.lower()=='shanba'and havo>=30:
+    #print('Cho\'milgani ketdik')
+#elif kun.lower()=='yakshanba' or kun.lower()=='shanba'and havo<30:
+    #print("Bugun uyda o'tiramiz")
+#else:
+    #print('Bugun ish kuni')
+
+
+
+
+
+
+
+
+#narh=15000
+#choy=True
+#salat=True
+
+#if choy and salat:
+    #narh=narh+10000
+#elif choy or salat :
+    #narh=narh+5000
+#print(f"Jami {narh} so'm")    
+
+
+
+
+
+
+#narh=15000
+#choy=1
+#salat=1
+#kompot=0
+#assorti=1
+#non=1
+#
+#if choy:
+    #print('Mijoz choy oldi')
+    #narh=narh+3000
+#if salat:
+    #print('Mijoz salat oldi')
+    #narh=narh+5000
+#if kompot:
+    #print('Mijoz kompot oldi')
+    #narh=narh+7000
+#if assorti:
+    #print('Mijoz assorti oldi')
+    #narh=narh+15000
+#if non:
+    #print('Mijoz non oldi')
+    #narh=narh+2000
+#print(f"Jami narh {narh} so'm")
+
+
+
+
+
+
+
+#menu=['osh','somsa','baliq','manti','kabob']
+#'osh ' in menu
+#ovqat=input('Nima ovqat yeysiz? ')
+#if ovqat.lower() in menu:
+    #print('Buyuyrtma qabul qilindi')
+#else:
+    #print('Afsuski bizda bunday ovqat yo\'q')
+
+
+
+
+
+
+menu=['osh','somsa','baliq','manti','kabob']
+buyurtmalar=['osh','shorva','ayron','manti']
+
+for taom in buyurtmalar:
+   if taom in menu:
+      print(f"Menuda {taom} bor")
+   else:
+      print(f"Kechirasiz mmenuda {taom} yoq")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
